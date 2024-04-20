47b2d79d55dd42d7143a87340071866a732e8c86
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2024-04-20 10:31:15 -0400

    Update resultColumn.tsx

--- src/features/search/uni/resultColumn.tsx
+++ src/features/search/uni/resultColumn.tsx
@@ -1,8 +1,8 @@
 // ResultsColumn.js
 import React from "react";
 import { IonCol } from "@ionic/react";
-import SearchResults from "./searchResults/index";
-import ScholarshipResult from "./scholarshipResults";
+import SearchResults from "./searchResult/index";
+import ScholarshipResult from "./scholarshipResult";
 import UniversityScholarshipTab from "../atoms/UniversityScholarshipTab";
 import { ThreadSkeleton } from "../../../components/packages/skeleton/threadSkeleton";
 

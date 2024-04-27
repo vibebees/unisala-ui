424d633b15b73af3eff25b9eedc5d3b76f0839f2
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2024-04-27 11:32:01 -0400

    setting up landing page

--- src/features/landingpage/FAQ.tsx
+++ src/features/landingpage/FAQ.tsx
@@ -94,8 +94,13 @@ const faqData = [
       ],
       'With Unisala, your organization isn’t just a club; it’s a gateway to lifelong connections and professional opportunities, enriching your educational journey and beyond.'
     ]
+  },
+  {
+    question: 'Who created the platform?',
+    answer: [
+      "The creators of Unisala were international students 🌍 who roamed the same halls as you might, studying in the U.S. at Southeastern Louisiana University 🎓. They faced the same exciting highs       and daunting lows of studying abroad and wanted to make sure others like them had an easier time.So, they came together and built Unisala, not just as a tool but as a friend to guide future students through their journey, just like they wished they had when they started. 🚀      "
+    ]
   }
-
 
 
 
@@ -106,22 +111,6 @@ const faqData = [
 ];
 /*
 
-{
-    question: 'How can current students benefit from Unisala?',
-    answer: 'Current students can utilize Unisala to enhance their academic and social experiences. The platform offers access to study resources, the ability to join or create student groups, and opportunities for networking with peers and alumni globally. Additionally, students can participate in mentorship programs, collaborate on projects, and share their own experiences to assist fellow and future students.'
-  },
-  {
-    question: 'Can current students participate in cultural events through Unisala?',
-    answer: 'Yes, current students are encouraged to participate in and even organize cultural and social events through Unisala. The platform helps promote these events, making it easier to gather interest and facilitate participation from the student community.'
-  },
-  {
-    question: 'How can alumni contribute to Unisala?',
-    answer: 'Alumni can contribute by sharing their experiences and advice through the platform’s forums, participating in mentorship programs, and helping to create a supportive network for new students.'
-  },
-  {
-    question: 'Is there a way for current students to track their academic progress on Unisala?',
-    answer: 'Unisala offers tools for students to track their academic progress, including features that allow them to set educational goals, monitor achievements, and assess their performance over time. This helps students stay on track and make adjustments to their study plans as needed.'
-  },
   {
     question: 'Who created the platform?',
     answer: 'Unisala was developed by a team of experienced educators and technology professionals with a deep understanding of the challenges faced by international students. The team is passionate about providing solutions that ease the educational and cultural transition for students studying abroad.'
@@ -133,6 +122,216 @@ const faqData = [
 
 */
 
+  const WhatIsUnisala = () => (
+    <div className='mb-4'>
+      <h6 className='mb-0'>
+        <button
+          className='relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-lg group'
+          onClick={() => handleCollapse(0)}
+        >
+          <h5>What is Unisala?</h5>
+          <i
+            className={`absolute right-0 pt-1 text-xs fa ${
+              activeCollapse === 0 ? 'fa-minus' : 'fa-plus'
+            } transition-opacity duration-300`}
+          ></i>
+        </button>
+      </h6>
+      <div
+        className={`overflow-hidden transition-all duration-300 ease-in-out ${
+          activeCollapse === 0 ? 'max-h-screen' : 'h-0'
+        }`}
+      >
+        <div className='p-4 text-sm leading-normal text-gray-600'>
+          <p>
+            Platform crafted to empower students through every phase of their
+            journey in the U.S. 🇺🇸✨
+          </p>
+          <ul className='list-disc pl-5 space-y-2'>
+            <li className='font-bold text-gray-800'>
+              Help in crucial decision-making students, helping them choose the
+              right major, university, and career path. 🎓🔍
+            </li>
+            <li className='font-bold text-gray-800'>
+              Enabling current students to connect with peers, alumni and
+              discover opportunities for collaboration and personal growth. 🤝💡
+            </li>
+            <li className='font-bold text-gray-800'>
+              Supports alumni in staying connected, networking, and mentoring,
+              reinforcing the cycle of success and guidance. 🌟🔄
+            </li>
+            <li className='font-bold text-gray-800'>
+              Dedicated space for vibrant knowledge sharing and collaborative
+              learning. 📖🤝
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+  const WhoCanUseUnisala = ({ isActive, onClick }) => (
+    <div className="mb-4">
+      <h6 className="mb-0">
+        <button
+          className="relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-lg group"
+          onClick={onClick}
+        >
+          <h5>Who can use Unisala?</h5>
+          <i className={`absolute right-0 pt-1 text-xs fa ${isActive ? 'fa-minus' : 'fa-plus'} transition-opacity duration-300`}></i>
+        </button>
+      </h6>
+      <div
+        className={`overflow-hidden transition-all duration-300 ease-in-out ${isActive ? 'max-h-screen' : 'h-0'}`}
+      >
+        <div className="p-4 text-sm leading-normal text-gray-600">
+          <p>Unisala is designed for a diverse audience:</p>
+          <ul className="list-disc pl-5 space-y-2">
+            <li className="font-bold text-gray-800">International students seeking guidance, and community. 🌍📚</li>
+            <li className="font-bold text-gray-800">Alumni looking to reconnect and network 🎓🤗</li>
+            <li className="font-bold text-gray-800">Organizations aiming to support and manage their international constituents. 🏛️🎉</li>
+          </ul>
+          <p>The platform offers resources and networking opportunities for each stage of the educational journey and includes</p>
+        </div>
+      </div>
+    </div>
+  );
+
+  const WhatsIncludedInPlatform = ({ isActive, onClick }) => (
+    <div className="mb-4">
+      <h6 className="mb-0">
+        <button
+          className="relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-lg group"
+          onClick={onClick}
+        >
+          <h5>What's included in the platform?</h5>
+          <i className={`absolute right-0 pt-1 text-xs fa ${isActive ? 'fa-minus' : 'fa-plus'} transition-opacity duration-300`}></i>
+        </button>
+      </h6>
+      <div
+        className={`overflow-hidden transition-all duration-300 ease-in-out ${isActive ? 'max-h-screen' : 'h-0'}`}
+      >
+        <div className="p-4 text-sm leading-normal text-gray-600">
+          <p>Unisala offers a suite of features to support your educational path in the U.S. Here’s what’s available:</p>
+          <ul className="list-disc pl-5 space-y-2">
+            <li className="font-bold text-gray-800">🏫 **University Database**: Access detailed profiles of over 6500 U.S. universities covering programs, professors, tuition, and campus life.</li>
+            <li className="font-bold text-gray-800">🎓 **Personalized Matchmaking**: Find the perfect university that fits your academic and personal preferences.</li>
+            <li className="font-bold text-gray-800">🗣️ **Community Interaction**: Join a global network for discussions, advice, and connections with students and alumni.</li>
+            <li className="font-bold text-gray-800">🌐 **Graduates Networks**: Connect with alumni for career insights and professional networking.</li>
+            <li className="font-bold text-gray-800">🎉 **Cultural & Social Events**: Get involved in events and webinars that enrich your student life.</li>
+          </ul>
+          <p>With Unisala, step into a portal that prepares you for academic excellence and a promising professional future.</p>
+        </div>
+      </div>
+    </div>
+  );
+
+  const CurrentStudentsBenefits = ({ isActive, onClick }) => (
+    <div className="mb-4">
+      <h6 className="mb-0">
+        <button
+          className="relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-lg group"
+          onClick={onClick}
+        >
+          <h5>How can current students benefit from Unisala?</h5>
+          <i className={`absolute right-0 pt-1 text-xs fa ${isActive ? 'fa-minus' : 'fa-plus'} transition-opacity duration-300`}></i>
+        </button>
+      </h6>
+      <div
+        className={`overflow-hidden transition-all duration-300 ease-in-out ${isActive ? 'max-h-screen' : 'h-0'}`}
+      >
+        <div className="p-4 text-sm leading-normal text-gray-600">
+          <p>Unisala enhances the academic and social life of students in the U.S. Here’s how:</p>
+          <ul className="list-disc pl-5 space-y-2">
+            <li className="font-bold text-gray-800">🤝 **Networking**: Connect with global peers to share ideas and collaborate.</li>
+            <li className="font-bold text-gray-800">📚 **Resources**: Access a wide range of study materials and tutorials.</li>
+            <li className="font-bold text-gray-800">🌟 **Mentorship**: Get career and personal development advice from alumni.</li>
+            <li className="font-bold text-gray-800">🎉 **Events**: Participate in cultural and social events for a well-rounded college experience.</li>
+            <li className="font-bold text-gray-800">📢 **Share Experiences**: Contribute your own insights and learn from others.</li>
+          </ul>
+          <p>Unisala prepares you for both academic success and a smooth transition into your career!</p>
+        </div>
+      </div>
+    </div>
+  );
+
+  const AlumniContribution = ({ isActive, onClick }) => (
+    <div className="mb-4">
+      <h6 className="mb-0">
+        <button
+          className="relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-lg group"
+          onClick={onClick}
+        >
+          <h5>How can alumni contribute to Unisala?</h5>
+          <i className={`absolute right-0 pt-1 text-xs fa ${isActive ? 'fa-minus' : 'fa-plus'} transition-opacity duration-300`}></i>
+        </button>
+      </h6>
+      <div
+        className={`overflow-hidden transition-all duration-300 ease-in-out ${isActive ? 'max-h-screen' : 'h-0'}`}
+      >
+        <div className="p-4 text-sm leading-normal text-gray-600">
+          <p>Alumni play a pivotal role in enriching the Unisala community. Here’s how they can make a difference:</p>
+          <ul className="list-disc pl-5 space-y-2">
+            <li className="font-bold text-gray-800">🌟 **Mentoring**: Offer guidance and advice to current students, helping them navigate their academic and career paths.</li>
+            <li className="font-bold text-gray-800">🤝 **Networking**: Engage with students and other alumni to build a strong professional network.</li>
+            <li className="font-bold text-gray-800">📚 **Resource Contribution**: Share valuable resources, such as lecture notes, career tips, and webinar access.</li>
+            <li className="font-bold text-gray-800">🎓 **University Reviews**: Provide firsthand insights and reviews about your alma mater to assist prospective students in making informed decisions.</li>
+            <li className="font-bold text-gray-800">🗣️ **Event Participation**: Participate in or host webinars, workshops, and talks that can benefit the student body.</li>
+          </ul>
+          <p>Alumni contributions help maintain a vibrant, supportive, and resourceful community at Unisala.</p>
+        </div>
+      </div>
+    </div>
+  );
+  const StudentOrganizationBenefit = ({ isActive, onClick }) => (
+    <div className="mb-4">
+      <h6 className="mb-0">
+        <button
+          className="relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-lg group"
+          onClick={onClick}
+        >
+          <h5>How can a student organization benefit from Unisala?</h5>
+          <i className={`absolute right-0 pt-1 text-xs fa ${isActive ? 'fa-minus' : 'fa-plus'} transition-opacity duration-300`}></i>
+        </button>
+      </h6>
+      <div
+        className={`overflow-hidden transition-all duration-300 ease-in-out ${isActive ? 'max-h-screen' : 'h-0'}`}
+      >
+        <div className="p-4 text-sm leading-normal text-gray-600">
+          <p>Student organizations can significantly enhance their influence and connectivity through Unisala. Here’s how they can harness the platform:</p>
+          <ul className="list-disc pl-5 space-y-2">
+            <li className="font-bold text-gray-800">🤝 **Robust Alumni Networks**: Build a strong community by reconnecting with alumni, turning a small current membership into a thriving network of engaged former and current members.</li>
+            <li className="font-bold text-gray-800">📣 **Event Promotion and Legacy Building**: Use Unisala to host and promote events and webinars, such as the recent computer science webinar by a Southeastern Louisiana University alumnus working at AT&T. This helps maintain a vibrant, active presence that appeals to both current members and alumni.</li>
+            <li className="font-bold text-gray-800">🌐 **Global and Historical Reach**: Expand your reach by connecting with hundreds or thousands of past members worldwide, showcasing the organization’s broad impact and history.</li>
+            <li className="font-bold text-gray-800">📚 **Resource and Knowledge Sharing**: Encourage a dynamic exchange of knowledge, with members and alumni contributing educational content, career tips, and more.</li>
+            <li className="font-bold text-gray-800">🔧 **Enhanced Visibility**: Increase your visibility on Unisala to attract more members and showcase your network’s strength and achievements.</li>
+          </ul>
+          <p>With Unisala, your organization isn’t just a club; it’s a gateway to lifelong connections and professional opportunities, enriching your educational journey and beyond.</p>
+        </div>
+      </div>
+    </div>
+  );
+  const PlatformCreators = ({ isActive, onClick }) => (
+    <div className="mb-4">
+      <h6 className="mb-0">
+        <button
+          className="relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-lg group"
+          onClick={onClick}
+        >
+          <h5>Who created the platform?</h5>
+          <i className={`absolute right-0 pt-1 text-xs fa ${isActive ? 'fa-minus' : 'fa-plus'} transition-opacity duration-300`}></i>
+        </button>
+      </h6>
+      <div
+        className={`overflow-hidden transition-all duration-300 ease-in-out ${isActive ? 'max-h-screen' : 'h-0'}`}
+      >
+        <div className="p-4 text-sm leading-normal text-gray-600">
+          <p>The creators of Unisala were international students 🌍 who roamed the same halls as you might, studying in the U.S. at Southeastern Louisiana University 🎓. They faced the same exciting highs and daunting lows of studying abroad and wanted to make sure others like them had an easier time. So, they came together and built Unisala, not just as a tool but as a friend to guide future students through their journey, just like they wished they had when they started. 🚀</p>
+        </div>
+      </div>
+    </div>
+  );
+
+
 // Rendering logic
 return (
   <motion.div
@@ -141,38 +340,14 @@ return (
     transition={{ type: 'spring', stiffness: 200, damping: 20 }}
     className="my-8"
   >
-    {faqData.map((item, index) => (
-      <div key={index} className="mb-4">
-        <h6 className="mb-0">
-          <button
-            className="relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-lg group"
-            onClick={() => handleCollapse(index)}
-          >
-            <h5>{item.question}</h5>
-            <i className={`absolute right-0 pt-1 text-xs fa ${activeCollapse === index ? 'fa-minus' : 'fa-plus'} transition-opacity duration-300`}></i>
-          </button>
-        </h6>
-        <div
-          className={`overflow-hidden transition-all duration-300 ease-in-out ${activeCollapse === index ? 'max-h-screen' : 'h-0'}`}
-        >
-          <div className="p-4 text-sm leading-normal text-gray-600">
-            {item.answer.map((part, idx) =>
-              Array.isArray(part) ? (
-                <ul key={idx} className="list-disc pl-5 space-y-2">
-                  {part.map((li, liIdx) => (
-                    <li key={liIdx} className="font-bold text-gray-800"> {/* Tailwind classes for bold and darker text */}
-                      {li}
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <p key={idx}>{part}</p>
-              )
-            )}
-          </div>
-        </div>
-      </div>
-    ))}
+
+    <WhatIsUnisala/>
+    <WhoCanUseUnisala isActive={activeCollapse === 1} onClick={() => handleCollapse(1)} />
+    <WhatsIncludedInPlatform isActive={activeCollapse === 2} onClick={() => handleCollapse(2)} />
+    <CurrentStudentsBenefits isActive={activeCollapse === 3} onClick={() => handleCollapse(3)} />
+    <AlumniContribution isActive={activeCollapse === 4} onClick={() => handleCollapse(4)} />
+    <StudentOrganizationBenefit isActive={activeCollapse === 5} onClick={() => handleCollapse(5)} />
+    <PlatformCreators isActive={activeCollapse === 6} onClick={() => handleCollapse(6)} />
   </motion.div>
 );
 

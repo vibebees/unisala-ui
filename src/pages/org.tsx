3398a67bb5bd221a41c72898eb23bd1ce331c907
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2024-05-18 13:38:04 -0400

    fix(create org/space available): layout changes and option to create space added

--- src/pages/org.tsx
+++ src/pages/org.tsx
@@ -1,15 +1,23 @@
-import React from "react"
-import Org from "../features/org/index"
-import FixedLayout from "@layouts/FixedLayout"
-import { FamousUniversities } from "@components/packages/famousUniversites"
-import { LeftSideBar } from "@components/packages/spacesAndOrgs"
+import React from 'react';
+import Org from '../features/org/index';
+import FixedLayout from '@layouts/FixedLayout';
+import { FamousUniversities } from '@components/packages/famousUniversites';
+import { LeftSideBar } from '@components/packages/spacesAndOrgs';
+import { Orgs } from '@components/packages/spacesAndOrgs/orgs';
+import CreateAOrg from '@components/packages/createSpace/CreateSpace';
 export default function OrgPage() {
   return (
     <FixedLayout
-    leftSidebar={<LeftSideBar />}
-    rightSidebar={<FamousUniversities />}
+      leftSidebar={
+        <LeftSideBar order={['Orgs']} />
+      }
+      rightSidebar={
+        <>
+          <CreateAOrg/>
+        </>
+      }
     >
-      <Org/>
-      </FixedLayout>
-  )
+      <Org />
+    </FixedLayout>
+  );
 }

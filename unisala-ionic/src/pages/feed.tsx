6c29842eeae23c7305c36e95cb2a1cf9ec7f51d8
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2024-06-20 10:46:33 -0400

    more on anayltics setup

--- unisala-ionic/src/pages/feed.tsx
+++ unisala-ionic/src/pages/feed.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import Home from "../features/home";
 import useViewDuration from "@hooks/useViewDuration";
 import ReactGA from "react-ga4";
+import { trackEvent } from "@components/analytics";
 
 const PageHome = () => {
   const { getAllViewTime, deleteAllViewTime } = useViewDuration();
@@ -13,11 +14,15 @@ const PageHome = () => {
 
     return () => {
       const getAllDuration = getAllViewTime();
-      ReactGA.event({
-        category: "feed_view",
-        action: "view_duration",
-        value: getAllDuration,
-      });
+
+      for (const key in getAllDuration) {
+        trackEvent({
+          action: "Feed_post_" + key + "_view_duration",
+          category: "Feed_view",
+          label: getAllDuration[ key ],
+          value: getAllDuration[ key ],
+        });
+      }
     };
   }, []);
 

f7c508db378c1fa827a70cdf40934b66e361ff22
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2024-06-26 16:39:21 -0400

    adding thread page in astro

---
+++ unisala-astro/src/components/thread/singlethread.tsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useRef } from "react";
+ import clsx from "clsx";
+
+interface ThreadExpandProps {
+  htmlText: string;
+  _id: string;
+  tags: Array<{
+    __typename?: "SpaceCategoryQuery";
+    _id: string;
+    name?: string;
+    parentId?: string;
+    image?: string;
+    description?: string;
+  } | null> | null;
+  videoURL?: string | null;
+}
+
+const ThreadExpand = ({
+  htmlText,
+  _id,
+  tags,
+  videoURL,
+}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [showSeeMore, setShowMore] = useState(true);
+  const TextRef = useRef(null);
+  let showSeeMoreOptionAtWordCount: number = 40;
+
+
+
+
+  useEffect(() => {
+    const wordCount = htmlText.split(" ").length;
+    setShowMore(wordCount > showSeeMoreOptionAtWordCount);
+  }, [htmlText, showSeeMoreOptionAtWordCount]);
+
+
+
+  const PostBodyText = () => (
+    <div
+      className="ql-editor cursor-pointer"
+      dangerouslySetInnerHTML={{ __html: htmlText }}
+      ref={TextRef}
+      onClick={(e: any) => {
+        if (e.target.tagName === "A") {
+          e.preventDefault();
+          // window.open(e.target.href, "_blank");
+        } else {
+        //   history.push(`/thread/${_id}`);
+        }
+      }}
+    />
+  );
+
+
+  return (
+    <div>
+      <PostBodyText />
+      {videoURL && (
+        <div className="mobile-video-style">
+          <iframe
+            src={videoURL}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen={true}
+            style={{ width: "100%", height: "auto", aspectRatio: "16 / 9" }}
+          ></iframe>
+        </div>
+      )}
+
+
+    </div>
+  );
+};
+
+export default ThreadExpand;

36e22d8bc55228b8311301fa0ed330589a30f97b
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2024-09-16 13:51:58 -0500

    adding changes on release

--- microfrontend/ecosystem.config.js
+++ microfrontend/ecosystem.config.js
@@ -1,19 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: 'unisala-ionic',
-      script: 'http-server',
-      args: '-p 3000 ./unisala-ionic/dist --proxy http://localhost:3000?',
-      env: {
-        NODE_ENV: 'production'
-      }
-    },
-    {
       name: 'unisala-astro',
       script: './unisala-astro/dist/start-astro-server.mjs',
       env: {
         NODE_ENV: 'production',
-        PORT: 3001
+        PORT: 3000
       }
     }
   ]

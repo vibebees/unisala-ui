22788e037119ae32449db69bc7738b3014aa524a
Prashant Basnet <30943807+prashantbasnet94@users.noreply.github.com>
2025-01-29 21:26:52 -0600

    chore(git): set up conventional commits with husky

--- unisala-astro/.commitlintrc.js
+++ unisala-astro/.commitlintrc.js
@@ -1,15 +1,15 @@
-module.exports = {
-    extends: ["@commitlint/config-conventional"],
+export default {
+    extends: ['@commitlint/config-conventional'],
     rules: {
-      "type-enum": [
+      'type-enum': [
         2,
-        "always",
-        ["feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore"]
+        'always',
+        ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore']
       ],
-      "scope-empty": [2, "never"], // Ensures that scope is not empty (fix() required)
-      "scope-case": [2, "always", "lower-case"], // Enforces lowercase scopes
-      "subject-empty": [2, "never"], // Ensures commit messages have subjects
-      "header-max-length": [2, "always", 72] // Limits commit message length
+      'scope-empty': [2, 'never'],
+      'scope-case': [2, 'always', 'lower-case'],
+      'subject-empty': [2, 'never'],
+      'header-max-length': [2, 'always', 72]
     }
   };
   

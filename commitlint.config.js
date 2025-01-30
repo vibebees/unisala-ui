4f7d8f9b3ee8b6bd46985666fc80f19da7d752f7
Prashant Basnet <30943807+prashantbasnet94@users.noreply.github.com>
2025-01-29 21:51:12 -0600

    chore: update app version

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,18 +2,18 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', [
-      'feat',    // new feature
-      'fix',     // bug fix
-      'docs',    // documentation
-      'style',   // formatting, etc; no code change
-      'refactor',// refactoring code
-      'test',    // adding tests, refactoring tests
-      'chore',   // updating build tasks, package manager configs, etc
-      'perf'     // performance improvements
+      'feat',
+      'fix',
+      'docs',
+      'style',
+      'refactor',
+      'test',
+      'chore',
+      'perf'
     ]],
-    'type-case': [2, 'always', 'lower'],
+    'type-case': [2, 'always', 'lowerCase'],  // Changed from 'lower' to 'lowerCase'
     'type-empty': [2, 'never'],
-    'scope-case': [2, 'always', 'lower'],
+    'scope-case': [2, 'always', 'lowerCase'], // Changed from 'lower' to 'lowerCase'
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 72]

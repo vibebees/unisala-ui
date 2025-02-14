6b7c4b24c1588c98e3f6953c7cd320164fe12c13
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2025-02-13 18:14:28 -0600

    fix: editor glitch fixing undo and redo loop

--- unisala-astro/src/components/newstory/templates/notePad.tsx
+++ unisala-astro/src/components/newstory/templates/notePad.tsx
@@ -41,7 +41,7 @@ const NotePad: React.FC<PostFormProps> = ({ }) => {
     const debouncedSaveDraft = useCallback(
         debounce((id: string, title: string, content: string) => {
             saveDraft(id, title, content);
-        }, 500),
+        }, 2000),
         [ saveDraft ]
     );
 
